ccf1f1d80cbc277471f1c16dd7cae25d645ca1aa
Asitha Navaratne <asithan476@gmail.com>
2024-12-02 10:37:06 +0530

    Added middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
 import express, { Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
 
 app.use(express.json());
 
